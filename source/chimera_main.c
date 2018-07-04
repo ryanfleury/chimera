8a52cb0f763b6e5e1d60f911470f7c740085263d
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-07-03 18:23:00 -0600

    Included files for i2c and CRT stuff

--- source/chimera_main.c
+++ source/chimera_main.c
@@ -69,6 +69,11 @@ Codebase Notes
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 // Local Code
 #include "chimera_utilities.c"
