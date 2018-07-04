8b57aca0c6699e3caddd5406b74075f67a3ae261
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-07-03 18:58:18 -0600

    Tried to fix compile error

--- source/chimera_debug.c
+++ source/chimera_debug.c
@@ -1,6 +1,18 @@
-#define debug_log(f, ...)  fprintf(stdout, "ERROR: " f "\n", __VA_ARGS__)
-#define error(f, ...)      fprintf(stderr, "ERROR: " f "\n", __VA_ARGS__)
 #define assert_statement(statement) if(!(statement)) _assert_fail_(__FILE__, __LINE__, #statement);
+
+void debug_log(const char *format, ...) {
+    va_list args;
+    va_start(args);
+    vsfprintf(stdout, format, args);
+    va_end(args);
+}
+
+void error(const char *format, ...) {
+    va_list args;
+    va_start(args);
+    vsfprintf(stderr, format, args);
+    va_end(args);
+}
 
 void _assert_fail_(const char *file, i32 line, 
                    const char *failed_statement) {
@@ -8,6 +20,6 @@ void _assert_fail_(const char *file, i32 line,
     error("Assertion \"%s\" failed in \"%s\" on line %i",
           failed_statement, file, line);
     
-    debug_log("%s", "Trying to exit cleanly...");
+    debug_log("Trying to exit cleanly...");
     exit(0);
 }
