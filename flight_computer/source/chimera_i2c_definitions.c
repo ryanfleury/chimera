63e2705a8de1d7f6b589db4da4662cecc39b17da
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-07-12 16:14:02 -0600

    Added temperature address

--- flight_computer/source/chimera_i2c_definitions.c
+++ flight_computer/source/chimera_i2c_definitions.c
@@ -1,3 +1,4 @@
-#define        CHIMERA_IMU_I2C_ADDRESS  0x68
-#define  CHIMERA_BAROMETER_I2C_ADDRESS  0x60
-#define           CHIMERA_I2C_FILENAME  "/dev/i2c-1"
+#define         CHIMERA_IMU_I2C_ADDRESS  0x68
+#define   CHIMERA_BAROMETER_I2C_ADDRESS  0x60
+#define CHIMERA_TEMPERATURE_I2C_ADDRESS  0x40
+#define            CHIMERA_I2C_FILENAME  "/dev/i2c-1"
