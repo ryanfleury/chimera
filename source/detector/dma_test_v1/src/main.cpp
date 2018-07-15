3e937c1b3b9e1319c9e3a48adb2edf6764ff2037
Nicholas Johnston <nijo5747@colorado.edu>
2018-07-15 17:14:35 -0600

    update dma test code.  currently not functional

--- source/detector/dma_test_v1/src/main.cpp
+++ source/detector/dma_test_v1/src/main.cpp
@@ -1,53 +1,55 @@
-//#include "RingBufferDMA.h"
-#include <stdint.h>
-
+// #include "RingBufferDMA.h"
+#include <DMAChannel.h>
+#include <Arduino.h>
 
 #define pinCount 16
 uint8_t portPins[pinCount] = {16, 17, 19, 18, 49, 50, 31, 32, 0, 1, 29, 30, 43, 46, 44, 45}; // Port B bits 0, 1, 2, 3, 4, 5, 10, 11, 16, 17, 18, 19, 20, 21, 22, 23
 
 uint16_t data = 0;
 
-//PORTA_PCR13 |= PORT_PCR_IRQC(1);
+DMAChannel dmachannel0;
 
-#include <Arduino.h>
-
+uint32_t toggleData = 0x0;
 
 void readADC() {
-  data |= digitalReadFast(portPins[0]);
-  data |= digitalReadFast(portPins[1]) << 1;
-  data |= digitalReadFast(portPins[2]) << 2;
-  data |= digitalReadFast(portPins[3]) << 3;
-  data |= digitalReadFast(portPins[4]) << 4;
-  data |= digitalReadFast(portPins[5]) << 5;
-  data |= digitalReadFast(portPins[6]) << 6;
-  data |= digitalReadFast(portPins[7]) << 7;
-  data |= digitalReadFast(portPins[8]) << 8;
-  data |= digitalReadFast(portPins[9]) << 9;
-  data |= digitalReadFast(portPins[10]) << 10;
-  data |= digitalReadFast(portPins[11]) << 11;
-  data |= digitalReadFast(portPins[12]) << 12;
-  data |= digitalReadFast(portPins[13]) << 13;
-  data |= digitalReadFast(portPins[14]) << 14;
-  data |= digitalReadFast(portPins[15]) << 15;
+	toggleData = 1;
 }
 
 extern "C" int main(void) {
-
-  // put your setup code here, to run once:
+  //delay(10000);
+  Serial.begin(115200);
+  Serial.println("hello");
+  bool state = 0;
   for (int i = 0; i < pinCount; i++) {
     pinMode(portPins[i],INPUT);
   }
-  Serial.begin(115200);
+  
+  //SCB_SHPR3 = 0x20200000;  // Systick = priority 32 (defaults to zero)
+  //PORTA_PCR0 |= PORT_PCR_IRQC(1); // Trigger DMA request on rising edge
   attachInterrupt(0, readADC, RISING);
-	pinMode(13, OUTPUT);
-	delay(10000);
-	Serial.println("hello");
-	while (1) {
-		if (data != 0) {
-			Serial.print("new data\t");
-			Serial.println(data,BIN);
-			data=0;
-		}
-	}
-}
+  
+  pinMode(13, OUTPUT);
+  digitalWriteFast(13,!state);
+  state = !state;
 
+  //dmachannel0.source(GPIOB_PDIR);
+  //dmachannel0.destination(toggleData);
+  
+  //dmachannel0.transferSize(4);
+  //dmachannel0.transferCount(1);
+  
+  //dmachannel0.triggerAtHardwareEvent(DMAMUX_SOURCE_PORTB);
+  //dmachannel0.enable();
+  
+  //noInterrupts();
+
+  while (1) {
+    if (toggleData != 0) {
+      //Serial.print("new data\t");
+      //Serial.println(toggleData,BIN);
+      digitalWriteFast(13,!state);
+      state = !state;
+      toggleData = 0x0;
+    }
+  }
+}
