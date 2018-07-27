342f2a098cc6e1d7e9ee500aba5778a7f2b8a494
Ryan Fleury <ryan.j.fleury@gmail.com>
2018-07-27 12:19:26 -0600

    Fixed i2c undeclared errors maybe?

--- source/flight_computer/chimera_fc_main.c
+++ source/flight_computer/chimera_fc_main.c
@@ -73,8 +73,8 @@ Codebase Notes
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
-#include <linux/i2c.h>
-//#include <linux/i2c-dev.h>
+//#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
